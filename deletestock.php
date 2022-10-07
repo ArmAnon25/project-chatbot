9ff3ad128a1f453ee2b1459548ab5483e363230c
project <anon.peteang@gmail.com>
2022-10-07 21:26:23 +0700

    update

--- deletestock.php
+++ deletestock.php
@@ -8,6 +8,7 @@ $result = mysqli_query($conn, $sql);
 
 if ($result) {
   header("location:welcomestock.php");
+  echo "ลบข้อมูลสำเร็จ";
   exit(0);
 } else {
   echo "ไม่สามารถลบได้ หรือ มีข้อผิดพลาดเกิดขึ้น";
